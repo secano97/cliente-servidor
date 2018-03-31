896f2de9e7f2da47f2f0df756e8910276b4c1373
secano97 <sebastiancanouribe@utp.edu.co>
2018-03-31 11:23:12 -0500

    print_result function was introduced

--- Homeworks/Homework3/sequential-version/kmeans.cpp
+++ Homeworks/Homework3/sequential-version/kmeans.cpp
@@ -6,7 +6,6 @@
 #include "load.hh"
 
 using namespace std;
-using c_dmat = const Matrix<double>;
 using dmat = Matrix<double>;
 using ulmat = Matrix<ulist>;
 using cmat = Matrix<cont>;
@@ -15,7 +14,7 @@ uint avail_films = 10; //17770; // movies amount
 uint avail_users = 2649429;     // users amount
 uint avail_centroids = 2;	      // centroids amount
 
-void cos_simil(cmat& dataset,c_dmat& centroids,ulmat& similarity){
+void cos_simil(const cmat& dataset,const dmat& centroids,ulmat& similarity){
 	/* This will calculate the cosain similarity between centroids and users */
 	for(uint user_id=0; user_id < dataset.numRows(); user_id++){
 		uint temp_cent_id = 0;
@@ -48,21 +47,21 @@ void cos_simil(cmat& dataset,c_dmat& centroids,ulmat& similarity){
 	}
 }
 
-void find_media(ulmat& similarity,cmat& dataset,dmat& new_centroids){
+void find_media(const ulmat& similarity,cmat& dataset,dmat& new_centroids){
 	/* This will calculate media between users into one set from similarity */
 	for(uint cent_id=0 ;cent_id < new_centroids.numRows(); cent_id++){
 		for(uint movie_id=0; movie_id < new_centroids.numCols(); movie_id++){
 			double user_rate_summary = 0.0;
 			for(auto& user_id : similarity.data[cent_id])
 				user_rate_summary += dataset.user_movie_rate(user_id,movie_id);
-			double media = user_rate_summary / similarity.data[cent_id].size();
+			double media = user_rate_summary / similarity.get_set_size(cent_id);
 			double &d = new_centroids.at(cent_id,movie_id);
 			d = media;
 		}
 	}
 }
 
-double eucli_dist(dmat& old_cent,dmat& new_cent){
+double eucli_dist(const dmat& old_cent,const dmat& new_cent){
 	/* This will calculate euclidian distance between two matrix */
 	double val = 0.0;
 	for(uint i=0; i<old_cent.numRows(); i++){
@@ -75,9 +74,9 @@ double eucli_dist(dmat& old_cent,dmat& new_cent){
 	return sqrt(val);
 }
 
-void print_result(ulmat& similarity){
+void print_result(const ulmat& similarity){
 	for(uint cent_id=0; cent_id < similarity.numRows(); cent_id++)
-		cout << cent_id << " : " << similarity.data[cent_id].size() << "\n";
+		cout << cent_id << " : " << similarity.get_set_size(cent_id) << "\n";
 }
 
 int main(int argc, char *argv[]){
