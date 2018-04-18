c774be31e295a69137b9479dbc0703e474806881
secano97 <sebastiancanouribe@utp.edu.co>
2018-04-17 22:34:11 -0500

    Pass data function was introduced

--- Homeworks/Homework3/sequential-version/load.hh
+++ Homeworks/Homework3/sequential-version/load.hh
@@ -8,10 +8,22 @@
 using namespace std;
 using mat = Matrix<cont>;        // setting an alias mat (matrix)
 
+void pass_data(mat& M1, mat& M2){
+  /* it will pass data between M1 and M2 */
+    uint user_id = 0;
+    const vector<cont>& unnormalized_data = M1.get_cont();
+    for(auto& user : unnormalized_data){
+      if(user.empty()) continue;
+      for(auto& movie : user)
+        M2.fill_like_dic(user_id,movie.first,movie.second);
+      user_id++;
+    }
+}
+
 void load_data(str fileName,const size_t& avail_users,mat& dataset){
   /* this function will load dataset into memory */
   ifstream file(fileName);        // open file
-  Matrix <cont>temp(avail_users); // building dataset of type container
+  mat temp(avail_users); // building dataset of type container
   str line;                       // one line will be loaded into line object
   uint movie_id = 0;              // movie id
 
@@ -34,6 +46,5 @@ void load_data(str fileName,const size_t& avail_users,mat& dataset){
   size_t real_avail_users = temp.real_avail_users();
   cout << "Users that at least rated a movie = " << real_avail_users << endl;
   dataset.resize(real_avail_users);
-  temp.pass_data(dataset);
-  temp.clear();
+  pass_data(temp,dataset);
 }
